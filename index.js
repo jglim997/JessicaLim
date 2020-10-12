83be2036f4227ef34f1da19b12a45592948b5886
jglim997 <72245500+jglim997@users.noreply.github.com>
2020-10-12 01:23:10 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function newtrashtalk(){
+    document.getElementById("img1").src = "images/trashtalk2.svg";
+    // 204 IS ORIGINAL
+}
+
+function trashtalk(){
+    document.getElementById("img1").src = "images/Group 228.svg";
+
+}
+
+function newpega(){
+    document.getElementById("img2").src = "images/pega2.svg";
+}
+
+function pega(){
+    document.getElementById("img2").src = "images/Group 229.svg";
+    // 194 is org
+
+}
+
+function newdou(){
+    document.getElementById("img3").src = "images/dou2.svg";
+}
+
+function dou(){
+    document.getElementById("img3").src = "images/Group 231.svg";
+    // 194 is org
+
+}
+
+function newred(){
+    document.getElementById("img6").src = "images/more to come2.svg";
+}
+function red(){
+    document.getElementById("img6").src = "images/more to come.svg";
+
+}
+
+// document.addEventListener("DOMContentLoaded",typeWriter());
+
+
+// function typeWriter(){
+//     var i = 0;
+//     var text = 'JOURNEY';
+//     var speed = 50;
+//     if (i < text.length){
+//         document.getElementById("type").innerHTML += text.charAt(i);
+//         i=i+1;
+//         setTimeout(typeWriter,speed);
+//     }
+// }
