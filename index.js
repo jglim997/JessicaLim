e41f16082ca2af3bdae76e0727cc5a28168556ac
jglim997 <jglim997@gmail.com>
2021-04-26 23:30:20 -0400

    hover changes

--- index.js
+++ index.js
@@ -1,64 +1,58 @@
-const text = ['websites','illustrations','pancakes'];
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
+// const text = ['websites','illustrations','pancakes'];
+// let count = 0;
+// let index = 0;
+// let currentText = '';
+// let letter = '';
 
-(function type(){
+// (function type(){
 
-    if(count === text.length){
-        count = 0;
+//     if(count === text.length){
+//         count = 0;
 
 
-    }
-    currentText = text[count];
-    letter = currentText.slice(0,++index);
-    document.querySelector('.typing').textContent = letter;
-    if(letter.length === currentText.length){
-        count++;
-        index = 0;
+//     }
+//     currentText = text[count];
+//     letter = currentText.slice(0,++index);
+//     document.querySelector('.typing').textContent = letter;
+//     if(letter.length === currentText.length){
+//         count++;
+//         index = 0;
         
-    }
-    setTimeout(type,350);
+//     }
+//     setTimeout(type,350);
     
     
     
 
-}());
-
-
-
-
-
-
+// }());
 
 
 function newtrashtalk(){
-    document.getElementById("img1").src = "images/Group 294.svg";
+    document.getElementById("img1").src = "images/Group_294.svg";
 
 }
 
 function trashtalk(){
-    document.getElementById("img1").src = "images/Group 288.svg";
+    document.getElementById("img1").src = "images/Group_288.svg";
 
 }
 
 function newpega(){
-    document.getElementById("img2").src = "images/group 2933.svg";
+    document.getElementById("img2").src = "images/Group_2933.svg";
 }
 
 function pega(){
-    document.getElementById("img2").src = "images/group 289.svg";
+    document.getElementById("img2").src = "images/Group_289.svg";
 
 
 }
 
 function newdou(){
-    document.getElementById("img3").src = "images/group 2955.svg";
+    document.getElementById("img3").src = "images/Group_2955.svg";
 }
 
 function dou(){
-    document.getElementById("img3").src = "images/group 290.svg";
+    document.getElementById("img3").src = "images/Group_287.svg";
 
 
 }
