8b77756a2b9c45a1c3dffb84b0a5628bd443cf0b
jglim997 <jglim997@gmail.com>
2022-03-01 16:40:46 -0500

    adding Amazon

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-// const text = ['websites','illustrations','pancakes'];
-// let count = 0;
-// let index = 0;
-// let currentText = '';
-// let letter = '';
+function newamazon(){
+    document.getElementById("img0").src = "images/Group_299.svg";
+}
 
-// (function type(){
-
-//     if(count === text.length){
-//         count = 0;
-
-
-//     }
-//     currentText = text[count];
-//     letter = currentText.slice(0,++index);
-//     document.querySelector('.typing').textContent = letter;
-//     if(letter.length === currentText.length){
-//         count++;
-//         index = 0;
-        
-//     }
-//     setTimeout(type,350);
-    
-    
-    
-
-// }());
-
+function amazon(){
+    document.getElementById("img0").src = "images/Group_298.svg";
+}
 
 function newtrashtalk(){
     document.getElementById("img1").src = "images/Group_294.svg";
-
 }
 
 function trashtalk(){
     document.getElementById("img1").src = "images/Group_288.svg";
-
 }
 
 function newpega(){
@@ -43,8 +20,6 @@ function newpega(){
 
 function pega(){
     document.getElementById("img2").src = "images/Group_289.svg";
-
-
 }
 
 function newdou(){
@@ -53,19 +28,4 @@ function newdou(){
 
 function dou(){
     document.getElementById("img3").src = "images/Group_287.svg";
-
-
 }
-
-// function newred(){
-//     document.getElementById("img6").src = "images/more to come2.svg";
-// }
-// function red(){
-//     document.getElementById("img6").src = "images/more to come.svg";
-
-// }
-
-// function myFunction() {
-//     var element = document.body;
-//     element.classList.toggle("dark-mode");
-// }
